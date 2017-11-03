1615237d55c859092b44a25faa42fb9e907de570
lp <843460950@qq.com>
2017-11-03 09:23:44 +0800

    2017.11.3

--- src/javas/SampleConnection2.java
+++ src/javas/SampleConnection2.java
@@ -1,22 +1,24 @@
 package javas;
 
-import java.sql.*;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
-/**
- * @author lp
- */
-public class SampleIntro {
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class SampleConnection2 {
     public static void main(String[] args) {
         try {
-            //可能会出现类找不到异常;java6支持驱动程序的自动加载，因此不需要显示的加载他们。
             Class.forName("com.mysql.jdbc.Driver");
+            //用DataSource(javax.sql库，只是提供了一个接口)类创建连接，
+            MysqlDataSource dataSource= new MysqlDataSource();
+            dataSource.setServerName("localhost");
+            dataSource.setDatabaseName("mylibrary");
+            Connection connection = dataSource.getConnection("root","843460950");
 
-            //connects to MySQL
-            Connection connection= DriverManager.getConnection("jdbc:mysql://localhost/mylibrary?useSSL=true","root","843460950");
-            //creates Statement object
             Statement statement=connection.createStatement();
             String query="SELECT publID, publName FROM publishers ORDER BY publName";
-            //
             ResultSet resultSet=statement.executeQuery(query);
             //结果集ResultSet维护一个表，该表的当前行可以获得。
             while (resultSet.next()){
@@ -26,7 +28,7 @@ public class SampleIntro {
             }
             resultSet.close();
         }catch (Exception e){
-            System.out.println("Error: " + e.toString() );
+
         }
     }
 }
